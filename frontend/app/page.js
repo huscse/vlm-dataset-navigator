5c112b0b026a7408d74164a471c5a3555ccc96c6
huscse <husnainkhaliq82@gmail.com>
2025-09-23 14:16:39 -0400

    fix frontend: page.js syntax

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,10 +2,7 @@ import Header from './components/Header';
 
 export default function Home() {
   return (
-    <div
-      className="min-h-screen flex flex-col bg-[linear-gradient(180deg,#111827_0%,#000000_45%,#1F2937_95%)]
-"
-    >
+    <div className="min-h-screen flex flex-col bg-[linear-gradient(180deg,#111827_0%,#000000_45%,#1F2937_95%)]">
       <Header />
     </div>
   );
